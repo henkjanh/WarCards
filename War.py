6e498bc02e08229e28b9f0af803cb15300693fcf
Henkjan Havenaar <hj@miniMac.havenaar.home>
2021-02-15 15:49:40 +0100

    first commit

---
+++ War.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+import random
+
+# CARD
+# SUIT,RANK,VALUE
+
+# GLOBAL
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':11, 'Queen':12, 'King':13, 'Ace': 14}
+
+class Card():
+
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+        self.value = values[rank]
+
+    def __str__(self):
+        return self.rank + " of " + self.suit
+
+class Deck():
+
+    def __init__(self):
+        self.all_cards = []
+
+        for suit in suits:
+            for rank in ranks:
+                # Create the Card Object
+                created_card = Card(suit,rank)
+                self.all_cards.append(created_card)
+
+    def shuffle(self):
+        random.shuffle(self.all_cards)
+
+    def deal_one(self):
+        return self.allcards.pop()
+
+class Player():
+
+    def __init__(self,name):
+        self.name = name
+        self.all_cards = []
+
+    def remove_one(self):
+        pass
+
+    def add_cards(self, new_cards):
+        pass
+
+    def __str__(self):
+        return f'Player {self.name} has {len(self.all_cards)} cards.'
+
+new_player = Player("Jose")
+
+print(new_player)
